4ce6c04d12bbb804bedb907a6c1d3b2fd08fa7ef
Felipe Cunha <felipecunha@felipes-imac.home>
2019-12-02 16:06:51 -0300

    configured navigation

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default function User() {
+export default function User(props) {
+  console.tron.log(props);
   return <View />;
 }
